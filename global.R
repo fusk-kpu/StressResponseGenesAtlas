e40eb732e391ef9010b1c620f57e280cca9273b4
fusk-kpu <s823631038@kpu.ac.jp>
2024-09-22 12:45:55 +0900

    test

--- global.R
+++ global.R
@@ -386,6 +386,7 @@ TemplateMatch <- function(input, output, session, query, selectRow, srga, cl, Br
   },
   server = FALSE)
   
+  
   ### Display template ####
   output$selected <- renderDataTable({
     datatable(
